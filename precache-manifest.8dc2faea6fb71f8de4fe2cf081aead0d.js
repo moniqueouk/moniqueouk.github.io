db4bd03bc6f93ba784b563fa6908c83d6318ccca
Monique Ouk <monique.ouk7@gmail.com>
2020-05-14 23:42:58 -0700

    Updates

---
+++ precache-manifest.8dc2faea6fb71f8de4fe2cf081aead0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ba294d9806a362126b2bfc2c2b4b62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "888029027d1747aa33e1",
+    "url": "/static/css/main.b7642062.chunk.css"
+  },
+  {
+    "revision": "02cbaae364e35464f641",
+    "url": "/static/js/2.43d15848.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.43d15848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "888029027d1747aa33e1",
+    "url": "/static/js/main.73359a63.chunk.js"
+  },
+  {
+    "revision": "5c47eba93e2885c28ac8",
+    "url": "/static/js/runtime-main.80088307.js"
+  }
+]);
