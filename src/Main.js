36012669bcf70e1d4017816b588a371252fa7ca1
Monique Ouk <monique.ouk7@gmail.com>
2023-08-25 16:35:49 -0700

    added Stanford page

--- src/Main.js
+++ src/Main.js
@@ -10,6 +10,7 @@ import FarmPage from "./FarmPage"; // Import the FarmPage component
 import SF2019 from "./SF2019"; // Import the SF2019 component
 import KNY from "./KNY"; // Import the SF2019 component
 import Reyes from "./Reyes";
+import Stanford from "./Stanford"
 
 class Main extends Component {
   render() {
@@ -24,6 +25,8 @@ class Main extends Component {
                     <NavLink exact to="/sf19" activeClassName="active-link">Bay Area</NavLink>
                     <NavLink exact to="/kny" activeClassName="active-link">hills</NavLink>
                     <NavLink exact to="/reyes" activeClassName="active-link">pt. reyes</NavLink>
+                    <NavLink exact to="/stanford" activeClassName="active-link">Stanford</NavLink>
+
 
                 </div>
             </div>
@@ -34,6 +37,7 @@ class Main extends Component {
             <Route path="/sf19" component={SF2019} />
             <Route path="/kny" component={KNY} /> 
             <Route path="/reyes" component={Reyes} /> 
+            <Route path="/stanford" component={Stanford} /> 
         </HashRouter>
     );
   }
---
+++ src/Stanford.js
@@ -0,0 +1,36 @@
+import React from "react";
+import './css/Farm.css';
+
+const Stanford = () => {
+    return (
+        <div id="farmpics">
+            <img src={ require('./img/stanford/fieldhockey.jpg') } alt="Two field hockey players after a game"/>
+            <img src={ require('./img/stanford/frosh2.jpg') } alt="Stanford UCLA football game"/>
+            <img src={ require('./img/stanford/bearial.jpg') } alt="Stanford dollies during the Bearial"/>
+            <img src={ require('./img/stanford/pip.jpg') } alt="Pip in Linda's sink"/>
+            <img src={ require('./img/stanford/minaleila2.jpg') } alt="Carmina and Leila taking a selfie :')"/>
+            <img src={ require('./img/ashland.jpg') } alt="Adam and Helen, listening to a story"/>
+            <img src={ require('./img/stanford/tahoe.jpg') } alt="Brian and Emily laughing at Lake Tahoe"/>
+            <img src={ require('./img/stanford/lakelag.jpg') } alt="Brian and Carmina at Lake Lag"/>
+            <img src="https://lh3.googleusercontent.com/pw/AIL4fc9V_s_scHkXw7gE0jT_wLVmvq3RsC148D-cDQsKT6tX6vYC6seeoh0EZUursyZTNhjNKWnL8CfurFHqSbQ67HVxOT9BLAV36on87Flbq3j70-gFxXtuIj4gXsFFMq2_wADDNkDlc7yUjw782t4sg4VH0w=w2048-h1365-s-no?authuser=0" alt="Ari Lennox singing at Stanford"/>
+            <img src="https://lh3.googleusercontent.com/pw/AIL4fc8mcZEicQEni9-CXV6lPSgvlFyZ1RQ65m20v979ZFs3m41FsvzzBIEMkCeJaAe0TeCwqRWKc4kBisa-rNTGLhusdi4TXCgJY_x5a-myEHHX3LtlC9BoFEpyZ1uQ2AsryZDTn_q1qIMEaXXRJnrNH5wXsQ=w2778-h1852-s-no?authuser=0" alt="Dr. Katie Bouman giving a talk at Stanford"/>
+            <img src={ require('./img/stanford/farm.jpg') } alt="Natalie slicing tomatos at the farm"/>
+            <img src={ require('./img/stanford/minmin.jpg') } alt="Cinnamon smiling :)"/>
+            <img src={ require('./img/stanford/frosh1.jpg') } alt="Cinnamon taking a selfie at Stanford-UCLA tailgate"/>
+            <img src={ require('./img/stanford/jack.jpg') } alt="Jack, shirtless during stingpong"/>
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+    );
+};
+
+export default Stanford;
